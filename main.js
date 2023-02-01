93a77762b0964c8cf497d283681191d5af5a1b64
ibielecka <iwona.k.michalik@gmail.com>
2023-02-01 14:59:07 +0100

    gotowe

---
+++ main.js
@@ -0,0 +1,9 @@
+function suma(firstNumber, secondNumber){
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+console.log(suma(10, 20));
+// let wynik = suma(firstNumber, secondNumber);
+
+// console.log(wynik);
